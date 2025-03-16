e501b5585f62f45a80963100a12f02d773f7a890
lukeeskinner <u1440351@umail.utah.edu>
2025-03-16 15:26:14 -0600

    api works

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Books from "./Components/Books";
 import { useState } from "react";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://www.googleapis.com/books/v1/volumes";
 
 const App = () => {
   const [books, setBooks] = useState([]);
   
   const onSearch = async (query) => {
-    const response = await fetch(`${API_URL}?q=${query}&key=${API_KEY}`);
-    const data = await response.json();
-    setBooks(data.items || []); 
+    try {
+      const response = await fetch(`${API_URL}?q=${query}&key=${API_KEY}`);
+      const data = await response.json();
+      setBooks(data.items || []);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+      setBooks([]);
+    }
   };
 
   return (
